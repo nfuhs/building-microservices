497086563a94bcee287f568d9927ea96276d2b35
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2020-02-14 18:53:19 +0100

    added handlers/hello.go

--- ep2/main.go
+++ ep2/main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/nfuhs/building-microservices/ep2/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Goodbye World")
-	})
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	http.ListenAndServe(":9090", nil)
 }
