5434d16d59369c9458e291953266143dc726f4b3
Norbert Fuhs <norbert.fuhs.xyz@gmail.com>
2020-02-14 13:18:27 +0100

    modified main.go

--- ep1/main.go
+++ ep1/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello microservices")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello world")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
